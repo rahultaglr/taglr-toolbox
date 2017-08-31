626094e6c2962296a1736f6c9ad5d81088dd5f60
pareshtaglr <31235567+pareshtaglr@users.noreply.github.com>
2017-08-31 15:31:04 +0530

    Update FontIcon.js
    Material icons changed to taglr

--- components/font_icon/FontIcon.js
+++ components/font_icon/FontIcon.js
@@ -4,7 +4,7 @@ import classnames from 'classnames';
 const FontIcon = ({ children, className, value, ...other}) => (
   <span
     data-react-toolbox='font-icon'
-    className={classnames({'material-icons': typeof value === 'string' || typeof children === 'string'}, className)}
+    className={classnames({'taglr-icons': typeof value === 'string' || typeof children === 'string'}, className)}
     {...other}
   >
     {value}
